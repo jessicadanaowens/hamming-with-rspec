65ac8e694dddc26cbaf95ceae0f28118938c2bb8
Jessica Owens <jessicadanaowens@gmail.com>
2014-06-26 10:22:16 -0600

    hamming with rspec

--- lib/dna_strand_pair.rb
+++ lib/dna_strand_pair.rb
@@ -1,3 +1,36 @@
 class DnaStrandPair
 
+  def initialize(strand_1, strand_2)
+    @strand_1 = strand_1
+    @strand_2 = strand_2
+  end
+
+
+  def hamming_distance
+
+    hamming_distance = Array.new
+
+    strand1_array = @strand_1.split('')
+    strand2_array = @strand_2.split('')
+
+    if strand1_array.length <= strand2_array.length
+      shortest = strand1_array
+      longest = strand2_array
+    else
+      shortest = strand2_array
+      longest = strand1_array
+    end
+
+    shortest.each_with_index do |element, index|
+      unless longest[index] == element
+        hamming_distance << element
+      end
+    end
+
+    hamming_distance.length
+
+  end
+
 end
+
+
